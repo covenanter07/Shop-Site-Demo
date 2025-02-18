1b7bf6e3ee9a8d2e0847024e10f77ecc7903ec74
HSUAN MIN <112792778+covenanter07@users.noreply.github.com>
2025-02-18 13:38:50 +0800

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,11 +1,6 @@
-
 <?php
 
 require __DIR__ . '/../vendor/autoload.php'; // Ensure the path to autoload is correct
-
-// Note: Remove dotenv loading as we're using Zeabur environment variables
-//$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-//$dotenv->load();
 
 $host = $_ENV['MYSQL_HOST'];
 $username = $_ENV['MYSQL_USERNAME'];
@@ -18,5 +13,4 @@ $con = mysqli_connect($host, $username, $password, $database, $port);
 if (!$con) {
     die("Connection Failed:" . mysqli_connect_error());
 }
-
 
