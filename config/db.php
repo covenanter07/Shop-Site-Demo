cb01e9cd67d876301d0a860b00f9666dbedd9dcb
HSUAN MIN <112792778+covenanter07@users.noreply.github.com>
2025-02-18 12:56:58 +0800

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,4 +1,3 @@
-
 <?php
 
 require __DIR__ . '/../vendor/autoload.php'; // Ensure the path to autoload is correct
@@ -7,13 +6,13 @@ require __DIR__ . '/../vendor/autoload.php'; // Ensure the path to autoload is c
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 
-
 $host = $_ENV['MYSQL_HOST'];
 $username = $_ENV['MYSQL_USERNAME'];
 $password = $_ENV['MYSQL_PASSWORD'];
 $database = $_ENV['MYSQL_DATABASE'];
+$port = $_ENV['MYSQL_PORT'] ?? 3306; // Use 3306 as default if MYSQL_PORT is not set
 
-$con = mysqli_connect($host, $username, $password, $database);
+$con = mysqli_connect($host, $username, $password, $database, $port);
 
 if (!$con) {
     die("Connection Failed:" . mysqli_connect_error());
