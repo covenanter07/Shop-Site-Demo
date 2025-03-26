200f8a8ecf283e4b8c92f031174be82010ce8465
HSUAN MIN <112792778+covenanter07@users.noreply.github.com>
2025-03-26 12:06:20 +0800

    Update index.php

--- index.php
+++ index.php
@@ -80,17 +80,13 @@ include('functions/userfunction.php');
                 <a href="category.php" class="text-white"><i class="fa fa-angle-right"></i>Our Collections</a>
             </div>
             <div class="col-md-3">
-                <h4 class="text-white">Address</h4>
+                <h4 class="text-white">Contact</h4>
                 <p class="text-white">
-                    #19, Howllod Stair,
-                    3th street, wer ploor,
-                    matetae, Misa.
+                    E-commerce Collaboration,
+                    Online Media Collaboration,
                 </p>
                 <a href="tel:+8975412367" class="text-white"> <i class="fa fa-phone"></i> +32 546273737</a><br>
-                <a href="mailto:ecs@gmail.com" class="text-white"> <i class="fa fa-envelope"></i> ecs@gmail.com</a>
-            </div>
-            <div class="col-md-6">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3280.64414927224!2d135.83762821471737!3d34.68892959135734!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x600139907a0876dd%3A0xf890ac3f9dd53c8f!2zVMWNZGFpLWpp!5e0!3m2!1szh-TW!2stw!4v1681054794941!5m2!1szh-TW!2stw" class="w-100" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <a href="mailto:ecompshop@gmail.com" class="text-white"> <i class="fa fa-envelope"></i> ecompshop@gmail.com</a>
             </div>
         </div>
     </div>
